cbfba3a9e32f65b5c63d38e138a29e112047b572
Nelson Valdés <nelson9612@hotmail.com>
2023-07-13 19:21:54 -0300

    arreglo del menú hamburguesa

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ const close__nav=document.querySelector("#nav__close")
 
 /* agrego los listening necesarios para que aparezca y desaparezca los iconos del nav bar */
 open_nav.addEventListener("click",() => {
-    nav.classList.add("nav__visible");
+    nav.classList.add("nav__list--visible");
     open_nav.classList.add("nav__open--hidden");
-    close__nav.classList.add("nav__button");
+    close__nav.classList.add("nav__close--visible");
 });
 
 close__nav.addEventListener("click",() => {
-    nav.classList.remove("nav__visible");
-    close__nav.classList.remove("nav__button");
-    open_nav.classList.add("nav__open--visible");
+    nav.classList.remove("nav__list--visible");
+    open_nav.classList.remove("nav__open--hidden");
+    close__nav.classList.remove("nav__close--visible");
+    
 });
