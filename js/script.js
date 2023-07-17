d6694013c1ef3176401e56c64d29861643e5bb22
Nelson Valdés <nelson9612@hotmail.com>
2023-07-17 19:53:01 -0300

    adapté la sección productos a mobile

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const nav= document.querySelector("#nav__mobile");
 const open_nav = document.querySelector("#nav__open");
 const close__nav=document.querySelector("#nav__close")
 
-/* agrego los listening necesarios para que aparezca y desaparezca los iconos del nav bar */
+/* agrego los listener necesarios para que aparezca y desaparezca los iconos del nav bar */
 open_nav.addEventListener("click",() => {
     nav.classList.add("nav__list--visible");
     open_nav.classList.add("nav__open--hidden");
